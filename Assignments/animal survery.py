ed103f3a74b06b8e7b6ee46db8a32e8c23c74bc5
JasperVetsch27 <826843@mystma.org>
2024-09-23 09:30:21 -0500

    2

---
+++ Assignments/animal survery.py
@@ -0,0 +1,6 @@
+one = input("What is your favorite animal?")
+two = input("Why do you like this animal?")
+three = input("Where do they live?")
+four = input("What is a fun fact about them?")
+five = input("What color are they?")
+print("Your favorite animal is" + one + " " + "because" + two + "." + "They live in" + three + " " + "and" + four + "." + "They are" + five +  ".")
