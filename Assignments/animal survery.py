811b1420ff800dfbfe59fd4d82651c80a6a831a7
JasperVetsch27 <826843@mystma.org>
2024-09-25 09:28:29 -0500

    Animal Survey Adding Comments

--- Assignments/animal survery.py
+++ Assignments/animal survery.py
@@ -1,6 +1,6 @@
-one = input("What is your favorite animal?\n")
-two = input("Why do you like this animal?\n")
-three = input("Where do they live?\n")
-four = input("What is a fun fact about them?\n")
-five = input("What color are they?\n")
-print("Your favorite animal is " + one + " " + "because " + two + "." + "They live in " + three + " " + "and " + four+ "." + "They are " + five +  ".")
+one = input("What is your favorite animal?\n") #Favorite animal of person 
+two = input("Why do you like this animal?\n")  #The reason why they like the animal 
+three = input("Where do they live?\n")          #Where the animal lives 
+four = input("What is a fun fact about them?\n") #Fun fact about their favorite animal 
+five = input("What color are they?\n")          #The color of their animal 
+print("Your favorite animal is " + one + " " + "because " + two + "." + "They live in " + three + " " + "and " + four+ "." + "They are " + five +  ".") #Final statemnet combining all of the persons answers
