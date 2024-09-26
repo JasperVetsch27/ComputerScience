ca8894c63c7e57faec157452612a839c6afb9370
JasperVetsch27 <826843@mystma.org>
2024-09-26 09:30:58 -0500

    MadLibRevised

--- Assignments/MadLib.py
+++ Assignments/MadLib.py
@@ -1,10 +1,14 @@
-adjective = input("Put a cool adjective in\n>")
-hero = input("Put your name in\n>")          #In the far away kingdom of [adjective] there lived king [name of hero]
-activity = input("Name an acitiy you like to do with friends\n>")  #They liked to [activity] and run around with friends 
-villain = input("Name someone you do not like\n>")          #One day the evil Dr. [name of villain] took the king's friends away
-tool = input("Put in a weapn or tool you'd use to defeat someone in battle\n>")   #King [name of hero] took his trusty [name of tool] and went to the evil Dr. lair
-             #The king confronted the villain with his [name of tool] and defeated [name of villain]
-         #He took his friends back to his kingdom and they [activity] together
+ #In the far away kingdom of [adjective] there lived king [name of hero]
+#They liked to [activity] and run around with friends
+#One day the evil Dr. [name of villain] took the king's friends away
+ #King [name of hero] took his trusty [name of tool] and went to the evil Dr. lair
+ #The king confronted the villain with his [name of tool] and defeated [name of villain]
+#He took his friends back to his kingdom and they [activity] together
+adjective = input("Put a cool adjective in\n>") #Name of the Kingdom
+hero = input("Put your name in\n>")        #Name of the person typing in answers
+activity = input("Name an acitiy you like to do with friends\n>")  #Activity the perosn likes to do 
+villain = input("Name someone you do not like\n>")      #Someone the person does not like
+tool = input("Put in a weapn or tool you'd use to defeat someone in battle\n>")   #The name of a tool
 print("In the far away kingdom of " + adjective + " there lived king " + hero + ".")
 print("They liked to " + activity + " and run around with friends.")
 print("One day the evil Dr. " + villain + " took the king's friends away.")
