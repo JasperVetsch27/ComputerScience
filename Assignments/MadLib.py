9ff159f627604ccca2321460006987a41cb69329
JasperVetsch27 <826843@mystma.org>
2024-09-26 09:27:30 -0500

    MadLib

---
+++ Assignments/MadLib.py
@@ -0,0 +1,14 @@
+adjective = input("Put a cool adjective in\n>")
+hero = input("Put your name in\n>")          #In the far away kingdom of [adjective] there lived king [name of hero]
+activity = input("Name an acitiy you like to do with friends\n>")  #They liked to [activity] and run around with friends 
+villain = input("Name someone you do not like\n>")          #One day the evil Dr. [name of villain] took the king's friends away
+tool = input("Put in a weapn or tool you'd use to defeat someone in battle\n>")   #King [name of hero] took his trusty [name of tool] and went to the evil Dr. lair
+             #The king confronted the villain with his [name of tool] and defeated [name of villain]
+         #He took his friends back to his kingdom and they [activity] together
+print("In the far away kingdom of " + adjective + " there lived king " + hero + ".")
+print("They liked to " + activity + " and run around with friends.")
+print("One day the evil Dr. " + villain + " took the king's friends away.")
+print("King " + hero + " took his trusty " + tool + " and went to the evil Dr. Lair." )
+print("The king confronted the villain with his " + tool + " and defeated " + villain + ".")
+print("He took his freinds back to his kingdom and they " + activity + " and ran together.")
+print("The End")
