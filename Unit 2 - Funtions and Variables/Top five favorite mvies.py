d360ef0b5864aad705f84ccaaec5736ae7e6c7a0
JasperVetsch27 <826843@mystma.org>
2024-09-27 09:44:29 -0500

    sept 27

---
+++ Unit 2 - Funtions and Variables/Top five favorite mvies.py
@@ -0,0 +1,17 @@
+def top_five_movies(name, movie_1, movie_2, movie_3, movie_4, movie_5):
+    print(name + "'s favorite movies")
+    print("1." + movie_1)
+    print("2." + movie_2)
+    print("3." + movie_3)
+    print("4." + movie_4)
+    print("5." + movie_5)
+name =  input("whats your name\n>")
+movie_1 = input("whats your first favorite movie\n>")
+movie_2 = input("whats your second favorite movie\n>")
+movie_3 = input("whats your third favorite movie\n>")
+movie_4 = input("whats your fourth favorite movie\n>")
+movie_5 = input("whats your fifth favorite movie\n>")
+
+top_five_movies(name, movie_1, movie_2, movie_3, movie_4, movie_5)
+
+
--- Unit 2 - Funtions and Variables/function.py
+++ Unit 2 - Funtions and Variables/function.py
@@ -1,5 +1,14 @@
-def greet():
-    print("Hello, World!")
-
+def greet():            #Define a new function 
+    print("Hello, World!")   #Indented line are inside the function
 print("catdog")
 greet()
+
+
+
+def add(x, y):
+    print(x + y)
+
+num_1 = float(input("What's the first number\n>"))
+num_2 = float(input("What's the second number\n>"))
+
+add(num_1, num_2)
