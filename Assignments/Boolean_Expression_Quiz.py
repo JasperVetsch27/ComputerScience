804d8b6f569148444b6df283acfcc4478f122aeb
JasperVetsch27 <826843@mystma.org>
2024-10-04 09:08:44 -0500

    Boolean_Quiz

---
+++ Assignments/Boolean_Expression_Quiz.py
@@ -0,0 +1,14 @@
+answer_1 = int(input("Give me a whole number less than 10\n>"))
+print(answer_1 < 10)
+
+answer_2 = int(input("What is 10 divided by 2\n>"))
+print(answer_2 == 5)
+
+answer_3 = int(input("what is 10 times 10\n>"))
+print(answer_3 == 100)
+
+answer_4 = int(input("what is 6(10+2)\n>"))
+print(answer_4 == 72)
+
+answer_5 = int(input("What is x equal to in the expression x <= 3(2+4)\n>"))
+print(answer_5 <= 18)
