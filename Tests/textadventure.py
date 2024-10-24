5b58ee09113869de392f01525c91b1a93cec1d6c
JasperVetsch27 <826843@mystma.org>
2024-10-24 09:47:58 -0500

    oct 24

--- Tests/textadventure.py
+++ Tests/textadventure.py
@@ -1,4 +1,4 @@
-Bond_With_Man = 0   # The bond you will have with a character in the game which will influence how the game ends 
+Bond = 0   # The bond you will have with a character in the game which will influence how the game ends 
 
 def Start_Adventure():  # Back round information to give context behind the story
     print("Your name is Bob and you have randomly come across a very suspicious looking facility with a dark mist around it")
@@ -59,6 +59,7 @@ def Inside_Creepy_Door():
 
 
 def Talk_With_Man():
+    global Bond 
     print("You see a man holding a flashlight at your face and he is also holding a shotgun")
     print("Still standing there he says, Omg I thought you were a monster, almost shot you there")
     print("What looks like to be an old man greets you and says, My name is Joe")
@@ -68,7 +69,7 @@ def Talk_With_Man():
     choice_4 = input("> ")  # Fourth choice in the story 
 
     if choice_4 == "1":
-        Bond_With_Man + 1   # Building bond with Man 
+        Bond + 1   # Building bond with Man 
         Monster_Encounter()
 
     elif choice_4 == "2":
@@ -78,6 +79,7 @@ def Talk_With_Man():
         Talk_With_Man()
 
 def Monster_Encounter():
+    global Bond 
     print("You have no more time to talk becuase a monster bursts through the door you just came through a moment ago")
     print("The old man tells you to run but not sure of what is the best option are frozen in place")
     print("1. You see a metal pipe on the gound and think to wack the monster with it")
@@ -86,7 +88,7 @@ def Monster_Encounter():
     choice_5 = input("> ")  # Fifth choice in the story
 
     if choice_5 == "1":
-        Bond_With_Man + 1   # Maximized bond with Joe, he now would fully trust you
+        Bond + 1  # Maximized bond with Joe, he now would fully trust you
         Through_The_Door()
 
     elif choice_5 == "2":
@@ -203,19 +205,70 @@ def Right_Pathway():
         Right_Pathway()
 
 def Real_Exit():
+    global Bond 
     print("As Joe is able to get the last chain off the door he goes through the door")
 
-    if Bond_With_Man == 2:  # Only happens if you have built a bond with Joe throughout the game
+    if Bond == 2:  # Only happens if you have built a bond with Joe throughout the game
         print("Joe tells you that he has opened the door and you go out the door with him")
         print("You both escape the facility and live long fulfilling lives")
         print("Game over")  # Fifth Ending 
 
-    elif Bond_With_Man < 2:
+    elif Bond < 2: # Haven't built enough bond with Joe
         print("Joe doesn't tell you that he has opened the door becuase he doesn't like you enough")
         print("He locks the door behind him and leaves you in the facility all alone")
         print("You are trapped in the facility forever and are never heard from again")
         print("Game Over")  #Sixth Ending 
 
+def Side_Room():
+    print("You see the monsters run past the door through the window")
+    print("You hear the screams of the old man in the distance and think that the worst has happened to him")
+    print("1. Explore more of the room you are hiding in")
+    print("2. Exit the room out into the hallway thinking that it is safe")
+
+    choice_12 = input("> ") # twelfth choice in the game
+
+    if choice_12 == "1":
+        Explore_Room()
+
+    elif choice_12 == "2":
+        Hallway()
+
+    else:
+        Side_Room()
+
+def Explore_Room():
+    print("As you walk through the dark room you hear the door open that you went though to get inside the room")
+    print("A monstery figure enters the room and sniffs around trying to find you")
+    print("1. Hide in a cuboard")
+    print("2. Confront the monster head on")
+
+    choice_13 = input("> ") # Thirtenth choice in the game 
+
+    if choice_13 == "1":
+        Meet_Up()
+
+    elif choice_13 == "2":
+        print("The monster lunges at you without giving you any time to try and fight back")
+        print("The monster has found its prey and you are dragged deeper into the facility never heard from again")
+        print("Game Over")  # Seventh Ending 
+
+def Meet_Up():
+    print("The cuboard was a really good hiding spot becuase the monster is never able to find you and eventually leaves")
+    print("You leave the room now and eventually find Joe alive and well")
+    print("It is short lived however because a monster comes after you both and you guys are forced to go through a door to escape him")
+    
+    Exit_Sign()
+
+def Hallway():
+    print("Outside there is a monster standing at the end of the hallway")
+    print("He is weirdly familiar to you and than you suddenly realize that it is Joe but he is now turned into a monster")
+    print("1. Try and get past him to get to the door behind him")
+    print("2. Turn around and go back to the front office to go inside the door that says safe on it")
+
+    choice_14 = input("> ") # Fourteenth choice 
+
+    if choice_14 == "1":
+        print("As you try and get around him he grabs you and spits this substance on you")
 
 
 
